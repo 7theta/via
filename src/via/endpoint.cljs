f58097fcbaca86fea48658fac059c15cf2dc1209
Tom Goldsmith <tom@7theta.com>
2020-08-23 13:21:40 -0400

    Resolves #10

--- src/via/endpoint.cljs
+++ src/via/endpoint.cljs
@@ -15,27 +15,38 @@
             [haslett.client :as ws]
             [haslett.format :as fmt]
             [utilis.fn :refer [fsafe]]
+            [utilis.map :refer [compact]]
             [utilis.types.string :refer [->string]]
-            [cljs.core.async :as a :refer [chan close! <! >! poll!]]
+            [cljs.core.async :as a :refer [chan close! <! >! poll! timeout]]
             [integrant.core :as ig]
             [goog.string :refer [format]]
             [goog.string.format]
             [clojure.string :as st]))
 
-(declare connect! disconnect! send! default-via-url)
+(declare connect! disconnect! send! default-via-url exponential-seq)
 
 (def interceptor)
 
 (defmethod ig/init-key :via/endpoint
-  [_ {:keys [url auto-connect connect-opts]
+  [_ {:keys [url
+             auto-connect
+             auto-reconnect
+             max-reconnect-interval
+             connect-opts]
       :or {auto-connect true
+           auto-reconnect true
+           max-reconnect-interval 5000
            url (default-via-url)}
       :as opts}]
   (let [endpoint {:url url
                   :stream (atom nil)
                   :subscriptions (atom {})
                   :control-ch (atom nil)
-                  :requests (atom {})}]
+                  :requests (atom {})}
+        connect-opts (compact
+                      (assoc connect-opts
+                             :auto-reconnect auto-reconnect
+                             :max-reconnect-interval max-reconnect-interval))]
     (set!
      interceptor
      (->interceptor
@@ -60,32 +71,47 @@
 
 (defn connect!
   ([endpoint] (connect! endpoint nil))
-  ([endpoint {:keys [params]}]
+  ([endpoint {:keys [params
+                     auto-reconnect
+                     max-reconnect-interval]}]
    (let [endpoint (endpoint)]
      (when-not @(:stream endpoint)
        (when-let [old-control-ch @(:control-ch endpoint)] (close! old-control-ch))
-       (let [stream (ws/connect (append-query-params (:url endpoint) params)
-                                {:format fmt/transit})
-             control-ch (chan)]
-         (reset! (:control-ch endpoint) control-ch)
-         (go
-           (let [{:keys [close-status source sink] :as stream} (<! stream)]
-             (reset! (:stream endpoint) stream)
-             (handle-event endpoint :open {:status :initial})
-             (loop []
-               (alt!
-                 control-ch (ws/close stream)
-                 source ([message]
-                         (if-not (nil? message)
-                           (case (:type message)
-                             :message (handle-event endpoint :message message)
-                             :reply (handle-reply endpoint message))
-                           (js/console.warn "via: nil message from server"))
-                         (recur))
-                 close-status ([status]
-                               (js/console.warn "via: connection closed" (pr-str status))
-                               (reset! (:stream endpoint) nil)
-                               (handle-event endpoint :close (merge {:status :forced} status))))))))))))
+       (go (try (let [connection-status (atom :disconnected)]
+                  (loop [backoff-sq (when auto-reconnect (exponential-seq 2 max-reconnect-interval))]
+                    (let [stream (ws/connect (append-query-params (:url endpoint) params)
+                                             {:format fmt/transit})
+                          control-ch (chan)]
+                      (reset! (:control-ch endpoint) control-ch)
+                      (when (= :disconnected
+                               (<! (go
+                                     (let [{:keys [close-status source sink] :as stream} (<! stream)]
+                                       (reset! (:stream endpoint) stream)
+                                       (when (ws/connected? stream)
+                                         (reset! connection-status :connected)
+                                         (handle-event endpoint :open {:status :initial}))
+                                       (loop []
+                                         (alt!
+                                           control-ch (do (ws/close stream) :shutdown)
+                                           source ([message]
+                                                   (if-not (nil? message)
+                                                     (case (:type message)
+                                                       :message (handle-event endpoint :message message)
+                                                       :reply (handle-reply endpoint message))
+                                                     (js/console.warn "via: nil message from server"))
+                                                   (recur))
+                                           close-status ([status]
+                                                         (when (not= :disconnected @connection-status)
+                                                           (reset! connection-status :disconnected)
+                                                           (reset! (:stream endpoint) nil)
+                                                           (handle-event endpoint :close (merge {:status :forced} status)))
+                                                         :disconnected)))))))
+                        (when auto-reconnect
+                          (let [reconnect-interval (first backoff-sq)]
+                            (<! (timeout reconnect-interval))
+                            (recur (rest backoff-sq))))))))
+                (catch js/Error e
+                  (js/console.error "Error occurred in via connection loop" e))))))))
 
 (defn disconnect!
   [endpoint]
@@ -169,3 +195,15 @@
        (cons url)
        (st/join "?")
        js/encodeURI))
+
+(defn- exponential-seq
+  ([base max-value]
+   (map #(min % max-value) (exponential-seq base)))
+  ([base]
+   (let [base (js/Math.abs base)]
+     (->> {:step 1 :value base}
+          (iterate
+           (fn [{:keys [step]}]
+             {:value (js/Math.pow base (inc step))
+              :step (inc step)}))
+          (map :value)))))
